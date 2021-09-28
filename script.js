246163db4af765c8bbbcfaa5f180b7e4dca53ea1
NixonXC <81410474+NixonXC@users.noreply.github.com>
2021-09-28 23:15:59 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById("inv").onclick = function ()
 {
-    window.location.href="#"
+    window.location.href="https://discord.com/api/oauth2/authorize?client_id=890931193015578636&permissions=536870387702&scope=bot%20applications.commands"
 };
 console.log("#")
